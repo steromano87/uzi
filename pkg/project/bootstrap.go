e379a2bfb5fb0dee09976e6f9f79fc53d1cb8a84
Stefano Romanò <rumix87@gmail.com>
2022-10-01 14:50:12 +0200

    Added project bootstrapper to create project structure from scratch

---
+++ pkg/project/bootstrap.go
@@ -0,0 +1,35 @@
+package project
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func Bootstrap(workingFolder string) error {
+	// Create required folders
+	foldersToCreate := []string{
+		ResultsFolder,
+		ProfilesFolder,
+		VariablesFolder,
+	}
+
+	for _, folder := range foldersToCreate {
+		err := os.MkdirAll(filepath.Join(workingFolder, folder), os.ModeDir)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create required files
+	configFile, err := os.Create(filepath.Join(workingFolder, ConfigurationFile))
+	if err != nil {
+		return err
+	}
+	defer func(configFile *os.File) {
+		_ = configFile.Close()
+	}(configFile)
+
+	_, err = configFile.WriteString(DefaultProjectConfigurationContent)
+
+	return err
+}
