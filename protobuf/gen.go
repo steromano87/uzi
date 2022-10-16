f53956c6679bcd72a5cbeff905c8217e4cf838b1
Stefano Romanò <rumix87@gmail.com>
2022-10-16 22:29:37 +0200

    Moved samples Proto files to a subfolder

--- protobuf/gen.go
+++ protobuf/gen.go
@@ -1,3 +1,4 @@
 package protobuf
 
 //go:generate sh -c "protoc --go_out=../pkg/message --go_opt=module=github.com/steromano87/harkonnen/v1/pkg/message *.proto"
+//go:generate sh -c "protoc --go_out=../pkg/message --go_opt=module=github.com/steromano87/harkonnen/v1/pkg/message **/*.proto"
