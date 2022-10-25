a744d5f59308206b04403a2d03e17291b81a80bc
Stefano Romanò <rumix87@gmail.com>
2022-10-26 01:24:28 +0200

    Added version package to allow compile-time set of version and commit hash while keeping field accessible from all packages

--- cmd/hark/hark.go
+++ cmd/hark/hark.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/steromano87/harkonnen/v1/pkg/version"
 	"os"
 )
-
-var Version = "master"
 
 var (
 	workingFolder string
@@ -17,10 +16,10 @@ var (
 var rootCmd = &cobra.Command{
 	Use: "hark",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Harkonnen Load Testing Engine - Version %s\n\n", Version)
+		fmt.Printf("Harkonnen Load Testing Engine - Version %s\n\n", version.Version)
 		println("Type 'hark help' to show the available options\n")
 	},
-	Version: Version,
+	Version: fmt.Sprintf("%s (%s)", version.Version, version.CommitHash),
 }
 
 func init() {
