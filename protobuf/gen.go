a19d21e4cbb569f06a905ad175c1f93b0e130dc4
Stefano Romanò <rumix87@gmail.com>
2022-11-05 17:27:26 +0100

    Removed old Protobuf files and message package (superseded by generated gRPC client and server packages)

--- protobuf/gen.go
+++
@@ -1,4 +0,0 @@
-package protobuf
-
-//go:generate sh -c "protoc --go_out=../pkg/message --go_opt=module=github.com/steromano87/harkonnen/v1/pkg/message *.proto"
-//go:generate sh -c "protoc --go_out=../pkg/message --go_opt=module=github.com/steromano87/harkonnen/v1/pkg/message **/*.proto"
