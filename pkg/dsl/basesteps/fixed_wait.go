a200c069d16ce053253cf541e918874cfb3fe7b3
Stefano Romanò <rumix87@gmail.com>
2023-03-08 19:29:12 +0100

    Make fixed wait interruptable with context

--- pkg/dsl/basesteps/fixed_wait.go
+++ pkg/dsl/basesteps/fixed_wait.go
@@ -1,6 +1,7 @@
 package basesteps
 
 import (
+	"context"
 	"github.com/rs/zerolog"
 	"github.com/steromano87/harkonnen/v1/pkg/dsl"
 	"time"
@@ -14,6 +15,12 @@ type FixedWait struct {
 func (w *FixedWait) Run(ctx dsl.Context) error {
 	logger := zerolog.Ctx(ctx).With().Str("step", "FixedWait").Logger()
 	logger.Info().Dur("amount", w.amount).Msg("Waiting")
-	time.Sleep(w.amount)
-	return nil
+
+	select {
+	case <-time.After(w.amount):
+		return nil
+
+	case <-ctx.Done():
+		return context.Cause(ctx)
+	}
 }
