2dc6aa8755cfda1bd12db76bed92044e8dfd49e1
Stefano Romanò <rumix87@gmail.com>
2023-03-09 01:27:18 +0100

    Ad beating heart struct to manage bidirectional heartbeat between cockpit and injector

---
+++ pkg/injector/beating_heart.go
@@ -0,0 +1,122 @@
+package injector
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/rs/zerolog"
+	"sync"
+	"time"
+)
+
+type BeatingHeart struct {
+	logger *zerolog.Logger
+
+	beatInterval     time.Duration
+	beatTimeout      time.Duration
+	beatTicker       *time.Ticker
+	beatTimeoutTimer *time.Timer
+
+	incomingBeatChan chan struct{}
+	outgoingBeatChan chan struct{}
+
+	beatMonitorsWG sync.WaitGroup
+
+	childCancelCauseFunc context.CancelCauseFunc
+}
+
+func NewBeatingHeart(beatInterval, beatTimeout time.Duration) (*BeatingHeart, error) {
+	if beatTimeout <= beatInterval {
+		return nil, errors.New(fmt.Sprintf("beat timeout (%s) cannot be smaller than beat interval (%s)", beatTimeout, beatInterval))
+	}
+
+	beatingHeart := new(BeatingHeart)
+	beatingHeart.beatInterval = beatInterval
+	beatingHeart.beatTimeout = beatTimeout
+	beatingHeart.incomingBeatChan = make(chan struct{})
+	beatingHeart.outgoingBeatChan = make(chan struct{})
+
+	return beatingHeart, nil
+}
+
+func (bh *BeatingHeart) Start(ctx context.Context) context.Context {
+	bh.logger = zerolog.Ctx(ctx)
+	childCtx, childCancelCauseFunc := context.WithCancelCause(ctx)
+	bh.childCancelCauseFunc = childCancelCauseFunc
+
+	bh.beatTicker = time.NewTicker(bh.beatInterval)
+	bh.beatTimeoutTimer = time.NewTimer(bh.beatTimeout)
+
+	go bh.incomingBeatMonitorLoop(ctx)
+	go bh.outgoingBeatSendLoop(childCtx)
+
+	return childCtx
+}
+
+func (bh *BeatingHeart) Wait() {
+	bh.beatMonitorsWG.Wait()
+}
+
+func (bh *BeatingHeart) IncomingBeat() {
+	bh.incomingBeatChan <- struct{}{}
+}
+
+func (bh *BeatingHeart) OutgoingBeat() <-chan struct{} {
+	return bh.outgoingBeatChan
+}
+
+func (bh *BeatingHeart) incomingBeatMonitorLoop(ctx context.Context) {
+	bh.beatMonitorsWG.Add(1)
+	defer bh.beatMonitorsWG.Done()
+
+	for {
+		select {
+		case <-bh.incomingBeatChan:
+			bh.contextualizedLogger().Debug().Msg("Received incoming heartbeat")
+			if !bh.beatTimeoutTimer.Stop() {
+				<-bh.beatTimeoutTimer.C
+			}
+
+			bh.beatTimeoutTimer.Reset(bh.beatTimeout)
+
+		case <-ctx.Done():
+			bh.contextualizedLogger().Info().Err(context.Cause(ctx)).Msg(
+				"Context canceled, exiting incoming beats monitor loop")
+			if !bh.beatTimeoutTimer.Stop() {
+				<-bh.beatTimeoutTimer.C
+			}
+			bh.childCancelCauseFunc(context.Cause(ctx))
+
+			return
+
+		case <-bh.beatTimeoutTimer.C:
+			bh.contextualizedLogger().Error().Msg("Incoming heartbeat timeout, canceling child context")
+			bh.childCancelCauseFunc(errors.New("incoming heartbeat timeout"))
+			return
+		}
+	}
+}
+
+func (bh *BeatingHeart) outgoingBeatSendLoop(ctx context.Context) {
+	bh.beatMonitorsWG.Add(1)
+	defer bh.beatMonitorsWG.Done()
+
+	for {
+		select {
+		case <-bh.beatTicker.C:
+			bh.contextualizedLogger().Debug().Msg("Sending heartbeat")
+			// Use async function to avoid blocking main goroutine if the channel is not read
+			go func() { bh.outgoingBeatChan <- struct{}{} }()
+
+		case <-ctx.Done():
+			bh.contextualizedLogger().Info().Err(context.Cause(ctx)).Msg(
+				"Context canceled, exiting outgoing beats send loop")
+			return
+		}
+	}
+}
+
+func (bh *BeatingHeart) contextualizedLogger() *zerolog.Logger {
+	logger := bh.logger.With().Str("component", "Beating Heart").Logger()
+	return &logger
+}
---
+++ pkg/injector/beating_heart_test.go
@@ -0,0 +1,115 @@
+package injector_test
+
+import (
+	"context"
+	"github.com/rs/zerolog"
+	"github.com/steromano87/harkonnen/v1/pkg/injector"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestNewBeatingHeart(t *testing.T) {
+	bh, err := injector.NewBeatingHeart(5*time.Second, 10*time.Second)
+	if assert.NoError(t, err) {
+		assert.IsType(t, &injector.BeatingHeart{}, bh)
+	}
+}
+
+func TestNewBeatingHeartWithInvalidInput(t *testing.T) {
+	_, err := injector.NewBeatingHeart(10*time.Second, 5*time.Second)
+	assert.Error(t, err)
+}
+
+func TestNewBeatingHeartWithInvalidInput2(t *testing.T) {
+	_, err := injector.NewBeatingHeart(5*time.Second, 5*time.Second)
+	assert.Error(t, err)
+}
+
+func TestRunningHeartWithoutBeatTimeout(t *testing.T) {
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+	consoleWriter := zerolog.NewConsoleWriter()
+	consoleWriter.TimeFormat = "2006-01-02T15:04:05.000000"
+	logger := zerolog.New(consoleWriter).With().Timestamp().Logger()
+
+	tempCtx, cancelFunc := context.WithCancel(context.TODO())
+	ctx := logger.WithContext(tempCtx)
+
+	bh, _ := injector.NewBeatingHeart(10*time.Millisecond, 20*time.Millisecond)
+	childCtx := bh.Start(ctx)
+	childContextCanceled := false
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Millisecond)
+
+		for {
+			select {
+			case <-ticker.C:
+				bh.IncomingBeat()
+
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	statusChangeFuncWG := sync.WaitGroup{}
+	go func() {
+		statusChangeFuncWG.Add(1)
+		<-childCtx.Done()
+		logger.Info().Msg("Child context canceled, changing variable status")
+		childContextCanceled = true
+		// logger.Info().Bool("childContextCanceled", childContextCanceled).Msg("Variable status")
+		statusChangeFuncWG.Done()
+	}()
+
+	time.Sleep(50 * time.Millisecond)
+	assert.False(t, childContextCanceled)
+	cancelFunc()
+	bh.Wait()
+	statusChangeFuncWG.Wait()
+	assert.True(t, childContextCanceled)
+}
+
+func TestRunningHeartWithBeatTimeout(t *testing.T) {
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+	consoleWriter := zerolog.NewConsoleWriter()
+	consoleWriter.TimeFormat = "2006-01-02T15:04:05.000000"
+	logger := zerolog.New(consoleWriter).With().Timestamp().Logger()
+
+	tempCtx, cancelFunc := context.WithCancel(context.TODO())
+	ctx := logger.WithContext(tempCtx)
+
+	bh, _ := injector.NewBeatingHeart(10*time.Millisecond, 20*time.Millisecond)
+	childCtx := bh.Start(ctx)
+	childContextCanceled := false
+
+	go func() {
+		ticker := time.NewTicker(30 * time.Millisecond)
+
+		for {
+			select {
+			case <-ticker.C:
+				bh.IncomingBeat()
+
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	statusChangeFuncWG := sync.WaitGroup{}
+	go func() {
+		statusChangeFuncWG.Add(1)
+		<-childCtx.Done()
+		logger.Info().Msg("Child context canceled, changing variable status")
+		childContextCanceled = true
+		// logger.Info().Bool("childContextCanceled", childContextCanceled).Msg("Variable status")
+		statusChangeFuncWG.Done()
+	}()
+
+	time.Sleep(50 * time.Millisecond)
+	cancelFunc()
+	assert.True(t, childContextCanceled)
+}
