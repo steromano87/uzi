178aebd5199a347e3fafb64ed73ff4eccca395d0
Stefano Romanò <rumix87@gmail.com>
2024-04-23 00:34:17 +0200

    Move generation comment in separate file

--- pkg/telemetry/server.go
+++ pkg/telemetry/server.go
@@ -5,8 +5,6 @@ import (
 	config "github.com/steromano87/harkonnen/v1/pkg/workspace"
 )
 
-//go:generate sh -c "protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative *.proto"
-
 type Server struct {
 	UnimplementedTelemetryServer
 
