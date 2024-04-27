461d0e3d4cb9d54ef72ead72cac9c36188bf9a16
Stefano Romanò <rumix87@gmail.com>
2024-04-27 15:28:35 +0200

    Add function to determine whether two Harkonnen versions are compatible

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,6 +1,28 @@
 package version
 
+import "github.com/Masterminds/semver/v3"
+
 var (
 	Version    = "0.1.0"
 	CommitHash = "N/A"
 )
+
+func IsCompatible(myVersion string, otherVersion string) bool {
+	mySemver, err := semver.NewVersion(myVersion)
+
+	// If one of the versions cannot be parsed, mark them as incompatible
+	if err != nil {
+		return false
+	}
+
+	otherSemver, err := semver.NewVersion(otherVersion)
+	if err != nil {
+		return false
+	}
+
+	if mySemver.Equal(otherSemver) {
+		return true
+	}
+
+	return false
+}
