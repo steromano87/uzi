49d066cd40de36c6b5dfe7dbba8355f1355ed395
Stefano Romanò <rumix87@gmail.com>
2024-09-01 16:35:59 +0200

    Add scheduler shutdown management when the elapsed time is greater that the max scenario duration

--- pkg/injector/scheduler.go
+++ pkg/injector/scheduler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/steromano87/harkonnen/v1/pkg/injector/schedule"
 	"github.com/steromano87/harkonnen/v1/pkg/log"
 	"github.com/steromano87/harkonnen/v1/pkg/syntheticuser"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"time"
 )
 
@@ -16,14 +17,16 @@ type Scheduler struct {
 	profile schedule.Profile
 	logger  zerolog.Logger
 
-	start  time.Time
-	ticker *time.Ticker
+	start           time.Time
+	ticker          *time.Ticker
+	shutDownTimeout time.Duration
 }
 
 func NewScheduler(roster *Roster, profile schedule.Profile) Scheduler {
 	scheduler := Scheduler{
-		roster:  roster,
-		profile: profile,
+		roster:          roster,
+		profile:         profile,
+		shutDownTimeout: 1 * time.Minute,
 	}
 	scheduler.SetLogger(zerolog.Nop())
 	return scheduler
@@ -58,6 +61,14 @@ func (s *Scheduler) Serve(ctx context.Context, updateInterval time.Duration) err
 		select {
 		case now := <-s.ticker.C:
 			elapsedTime := now.Sub(s.start)
+			if elapsedTime > s.profile.TotalDuration() {
+				s.logger.Info().Dur(
+					"totalDuration",
+					s.profile.TotalDuration(),
+				).Dur("currentDuration", elapsedTime).Msg("Load profile end reached, exiting")
+				return s.waitForUsersShutdown(ctx)
+			}
+
 			s.logger.Debug().Dur("elapsedTime", elapsedTime).Msg("Updating total expected running users")
 			totalExpectedUsers := s.profile.At(elapsedTime)
 			usersQuotas := s.roster.SplitQuotasByWeight(totalExpectedUsers)
@@ -66,16 +77,7 @@ func (s *Scheduler) Serve(ctx context.Context, updateInterval time.Duration) err
 			}
 
 		case <-ctx.Done():
-			s.logger.Info().AnErr("reason", context.Cause(ctx)).Msg("Shutdown requested, scaling all running users to zero")
-			scaledUsers := make(map[string]uint64)
-			for _, userId := range s.roster.Keys() {
-				scaledUsers[userId] = uint64(0)
-			}
-			if err := s.updateActiveUsers(context.TODO(), scaledUsers); err != nil {
-				s.logger.Error().Err(err).Msg("cannot scale active users to zero")
-				return err
-			}
-			return nil
+			return s.waitForUsersShutdown(context.TODO())
 		}
 	}
 }
@@ -101,4 +103,49 @@ func (s *Scheduler) updateActiveUsers(ctx context.Context, quotas map[string]uin
 	}
 
 	return nil
+}
+
+func (s *Scheduler) waitForUsersShutdown(ctx context.Context) error {
+	s.logger.Info().Msg("Waiting for all active users to shutdown")
+	scaledUsers := make(map[string]uint64)
+	for _, userId := range s.roster.Keys() {
+		scaledUsers[userId] = uint64(0)
+	}
+	if err := s.updateActiveUsers(ctx, scaledUsers); err != nil {
+		s.logger.Error().Err(err).Msg("cannot scale active users to zero")
+		return err
+	}
+
+	timeoutCtx, cancelFunc := context.WithTimeout(ctx, s.shutDownTimeout)
+
+	for {
+		select {
+		case <-timeoutCtx.Done():
+			cancelFunc()
+			return nil
+
+		default:
+			var totalNonStoppedUsers uint64
+			s.roster.Each(func(agentId string, entry RosterEntry) {
+				activeUsersResponse, err := entry.Client.GetSyntheticUserCounters(ctx, &emptypb.Empty{})
+				if err != nil {
+					s.logger.Error().Err(err).Str("agentId", agentId).Msg("cannot get synthetic user counters, skipping to next agent")
+				}
+				totalNonStoppedUsers += activeUsersResponse.GetSetupInProgress() +
+					activeUsersResponse.GetRunning() +
+					activeUsersResponse.GetGracefullyShuttingDown() +
+					activeUsersResponse.GetTeardownInProgress()
+			})
+
+			s.logger.Debug().Uint64("activeUsers", totalNonStoppedUsers).Msg("Users status counters updated")
+
+			if totalNonStoppedUsers == 0 {
+				cancelFunc()
+				return nil
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+
+	}
 }
