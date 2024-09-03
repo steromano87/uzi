e0fd8a7cf224cef48edeef2daea07d31f3892843
Stefano Romanò <rumix87@gmail.com>
2024-09-03 22:19:27 +0200

    Add transaction storage after its execution

--- pkg/dsl/base/transaction.go
+++ pkg/dsl/base/transaction.go
@@ -2,14 +2,19 @@ package base
 
 import (
 	"github.com/steromano87/harkonnen/v1/pkg/dsl"
+	"github.com/steromano87/harkonnen/v1/pkg/telemetry"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"time"
 )
 
 type Transaction struct {
 	name  string
 	steps []dsl.Step
-	start time.Time
-	end   time.Time
+
+	start      time.Time
+	end        time.Time
+	successful bool
 }
 
 func (t *Transaction) Run(ctx dsl.Context) error {
@@ -17,17 +22,34 @@ func (t *Transaction) Run(ctx dsl.Context) error {
 
 	logger.Info().Msg("Transaction started")
 	t.start = time.Now()
+
+	// Defer save function to ensure that the end timestamp is the very final instant
 	defer func() {
 		t.end = time.Now()
 		logger.Info().Dur("duration", t.Duration()).Msg("Transaction ended")
+
+		transaction := &telemetry.Transaction{
+			Name:       t.name,
+			Start:      timestamppb.New(t.start),
+			End:        timestamppb.New(t.end),
+			Duration:   durationpb.New(t.Duration()),
+			Successful: t.successful,
+		}
+
+		if err := ctx.StoreTransaction(transaction); err != nil {
+			logger.Error().Err(err).Str("transactionName", t.name).Msg("Encountered an error while storing transaction")
+		}
 	}()
+
 	for _, step := range t.steps {
 		err := step.Run(ctx)
 		if err != nil {
+			t.successful = false
 			return err
 		}
 	}
 
+	t.successful = true
 	return nil
 }
 
