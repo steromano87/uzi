a9490d735e4fa3cebc3bcbc501f459339836bf45
Stefano Romanò <rumix87@gmail.com>
2024-09-15 01:02:24 +0200

    Add sample storage for fixed wait step

--- pkg/dsl/base/fixed_wait.go
+++ pkg/dsl/base/fixed_wait.go
@@ -3,6 +3,9 @@ package base
 import (
 	"context"
 	"github.com/steromano87/harkonnen/v1/pkg/dsl"
+	"github.com/steromano87/harkonnen/v1/pkg/telemetry"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"time"
 )
 
@@ -15,18 +18,23 @@ func (w *FixedWait) Run(ctx dsl.Context) error {
 	logger := ctx.Logger.With().Str("step", "FixedWait").Logger()
 	logger.Info().Dur("amount", w.amount).Msg("Waiting")
 
-	/*defer func() {
-		_ = ctx.StoreSample(&telemetry.Sample{
+	defer func() {
+		sample := &telemetry.Sample{
 			Timestamp:       timestamppb.Now(),
-			SyntheticUserId: "",
-			Duration:        durationpb.New(w.amount),
+			SyntheticUserId: ctx.SynthUserId,
 			Name:            w.description,
+			Kind:            "FixedWait",
+			Duration:        durationpb.New(w.amount),
 			SentBytes:       0,
 			ReceivedBytes:   0,
-			Kind:            "wait",
+			IsWait:          true,
 			SampleData:      nil,
-		})
-	}()*/
+		}
+
+		if err := ctx.StoreSample(sample); err != nil {
+			ctx.Logger.Error().Err(err).Msg("Encountered an error while storing sample")
+		}
+	}()
 
 	select {
 	case <-time.After(w.amount):
